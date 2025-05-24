245b2c9d96173e1a4f90bb45c7166f3892e1649d
Usman-Click <47116342+Usman-Click@users.noreply.github.com>
2025-05-24 20:48:43 +0100

    Updates intro text

--- client/src/components/Introduction.tsx
+++ client/src/components/Introduction.tsx
@@ -1,12 +1,12 @@
 export default function Introduction() {
   return (
-    <section className="py-10 md:py-16 section-darker mobile-p-reduced">
+    <section className="py-8 md:py-10">
       <div className="container-custom">
-        <div className="max-w-3xl mx-auto">
+        <div className="mx-auto">
           <div className="text-center mb-6 md:mb-10">
             <p className="text-center leading-relaxed px-2">
-              <span className="text-xl sm:text-2xl md:text-3xl text-blue-highlight font-bold block mb-4">
-                Novetiv is your AI-powered social media companion, built to transform how brands engage, grow, manage online presence & maximize brand influence.
+              <span className="text-xl sm:text-2xl md:text-5xl text-blue-highlight font-bold block mb-4 hover:text-[#A0A7E6]">
+                Novetiv is your AI-powered social media<br></br>companion, built to transform how<br></br>brands engage, grow, manage<br></br>online presence & maximize<br></br>brand influence.
               </span>
             </p>
           </div>
